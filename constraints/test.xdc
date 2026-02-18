dc5a8c5cdea480f28d93fcc3aab7ed853d8f10fa
Hariram-Laptop <ecisiisc@google.com>
2026-02-18 16:17:27 +0530

    loopback test added

---
+++ constraints/test.xdc
@@ -0,0 +1,15 @@
+## Clock input (onboard 100 MHz oscillator, 100 MHz at FPGA pin E3)
+set_property PACKAGE_PIN F4 [get_ports clk]
+set_property IOSTANDARD LVCMOS33 [get_ports clk]
+
+## External LED (connect to header P5 pin 57, FPGA pin N15)
+set_property PACKAGE_PIN R18 [get_ports led_out]
+set_property IOSTANDARD LVCMOS33 [get_ports led_out]
+
+## RX
+set_property PACKAGE_PIN B18 [get_ports rx]
+set_property IOSTANDARD LVCMOS33 [get_ports rx]
+
+##TX
+set_property PACKAGE_PIN A18 [get_ports tx]
+set_property IOSTANDARD LVCMOS33 [get_ports tx]
---
+++ design/led_blinking.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/21/2025 04:40:25 PM
+// Design Name: 
+// Module Name: led_blinking
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module led_blink (
+    input wire clk,      // 100 MHz clock from onboard oscillator
+    output reg led_out,  // connect to external LED via header pin
+    input tx,
+    output rx 
+);
+ 
+    reg [25:0] counter = 0;
+ 
+    always @(posedge clk) begin
+        counter <= counter + 1;
+        led_out <= counter[25];  
+        // This divides 100 MHz clock down to ~1.5 Hz (blinking every ~0.7s)
+    end
+ assign rx = tx;
+endmodule
